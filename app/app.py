1cd8d02028f464f979deb2ce1cd69b622ca6f198
beastguhan <guhanmg25@gmail.com>
2025-11-10 06:04:40 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ import os
 
 app = Flask(__name__)
 
-# Connect to Redis
+# Redis config
 redis_host = os.getenv('REDIS_HOST', 'redis')
 redis_port = int(os.getenv('REDIS_PORT', 6379))
 r = redis.Redis(host=redis_host, port=redis_port, decode_responses=True)
@@ -18,4 +18,5 @@ def home():
     return f"Hello from Flask! You have visited this page {visits} times."
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    # Flask inside container must run on port 5000
+    app.run(host='0.0.0.0', port=5000)
